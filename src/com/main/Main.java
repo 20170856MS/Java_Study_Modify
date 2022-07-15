94b46152596424f05f486b88a5fe17d106e55084
misnsuk <fpqk1004@naver.com>
2022-07-15 16:06:10 +0900

    0715_3

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,6 +3,7 @@ package com.main;
 import java.util.Scanner;
 
 import com.terran.Marine;
+import com.unit.Unit;
 
 
 public class Main {
@@ -13,6 +14,20 @@ public class Main {
 		m1.setColor("black");
 		m1.info();
 		m1.move();
+		
+		Unit unit = m1;
+		//Marine is a Unit
+		//Unit이 접근 가능한건 자신이 알고있는 것 까지
+		
+		System.out.println(unit.getHp());
+		System.out.println(unit.getColor());
+		System.out.println(unit.getName());
+		System.out.println(unit.getClass());
+	
+		
+		m1 = (Marine)unit;  //형변환
+		System.out.println(m1.getWeapon());
 	}
+	
 
 }
